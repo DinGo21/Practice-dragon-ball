6ba7b6f9d8a3fd350f972536e5a57cd0f69de3a7
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-05 10:55:35 +0100

    feat: Added javascript code to create cards

---
+++ js/index.js
@@ -0,0 +1,79 @@
+const charactersRequest = "https://dragonball-api.com/api/characters?limit=58";
+const planetsRequest = "https://dragonball-api.com/api/planets?limit=20";
+
+async function fetchJson(request)
+{
+	const response = await fetch(request);
+
+	if (response.ok)
+	{
+		return await response.json();
+	}
+	console.error("Error: failed to fetch request: ", request);
+	return null;
+}
+
+function createCharacterCards({image, name, race, gender, ki, maxKi, affiliation})
+{
+	return `
+		<div class="character">
+			<img class="characterImage" src="${image}" alt="${name}"/>
+			<div class="characterCard">
+				<h4 class="characterName">${name}</h4>
+				<p class="characterGenderRace">${race} - ${gender}</p>
+				<div class="characterInfo">
+					<h5>Base Ki</h5>
+					<p>${ki}</p>
+				</div>
+				<div class="characterInfo">
+					<h5>Max Ki</h5>
+					<p>${maxKi}</p>
+				</div>
+				<div class="characterInfo">
+					<h5>Affiliation</h5>
+					<p>${affiliation}</p>
+				</div>
+			</div>
+		</div>
+	`;
+}
+
+function createPlanetCards({image, name, isDestroyed})
+{
+	return `
+		<div class="planet">
+			<img class="planetImage" src="${image}"/>
+			<div class="planetCard">
+				<h4 class="planetName">${name}</h4>
+			</div>
+		</div>
+	`;
+}
+
+async function displayElementCards(elementId, request)
+{
+	const element = document.getElementById(elementId);
+	const elementData = await fetchJson(request);
+	let	  elementCards;
+
+	if (!element)
+	{
+		return console.error("Error: couldn't get element by id:", elementId);
+	}
+	if (!elementData || !elementData.items)
+	{
+		return console.error("Error: couldn't read data for element:", elementId);
+	}
+	if (elementId === "characters")
+	{
+		elementCards = elementData.items.map(createCharacterCards).join('');
+	}
+	if (elementId === "planets")
+	{
+		elementCards = elementData.items.map(createPlanetCards).join('');
+	}
+	element.innerHTML = elementCards;
+}
+
+displayElementCards("characters", charactersRequest);
+displayElementCards("planets", planetsRequest);
